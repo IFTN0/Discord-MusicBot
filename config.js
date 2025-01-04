aec1568ae2456d050f73091c6819ad59327d3a13
ISSAM_FTN <98432254+IFTN0@users.noreply.github.com>
2025-01-04 16:41:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
-	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || "", //- Bot's Token
-	clientId: process.env.clientId || "", //- ID of the bot
-	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
+	adminId: "770278507334729728", //- Replace UserId with the Discord ID of the admin of the bot
+	token: process.env.token || "ODY5MjA4OTExMTIxNjMzMzEw.GokMht.0ZH2DC2DJT9Wgq4scDXJ3jUINnH5WQnr-KunyA", //- Bot's Token
+	clientId: process.env.clientId || "869208911121633310", //- ID of the bot
+	clientSecret: process.env.clientSecret || "IKbgcoAGQz3i-PwKSWAcTv2yJe29m3Ag", //- Client Secret of the bot
 	port: 4200, //- Port of the API and Dashboard
 	scopes: ["identify", "guilds", "applications.commands"], //- Discord OAuth2 Scopes
 	inviteScopes: ["bot", "applications.commands"], // Invite link scopes
 	serverDeafen: true, //- If you want bot to stay deafened
 	defaultVolume: 100, //- Sets the default volume of the bot, You can change this number anywhere from 1 to 100
 	supportServer: "https://discord.gg/sbySMS7m3v", //- Support Server Link
-	Issues: "https://github.com/SudhanPlayz/Discord-MusicBot/issues", //- Bug Report Link
+	Issues: "https://github.com/IFTN0/Discord-MusicBot/issues", //- Bug Report Link
 	permissions: 277083450689, //- Bot Inviting Permissions
 	disconnectTime: 30000, //- How long should the bot wait before disconnecting from the voice channel (in miliseconds). Set to 1 for instant disconnect.
 	twentyFourSeven: false, //- When set to true, the bot will never disconnect from the voice channel
@@ -19,7 +19,7 @@ module.exports = {
 	autoPause: true, //- When set to true, music will automatically be paused if everyone leaves the voice channel
 	autoLeave: false, //- When set to true, the bot will automatically leave when no one is in the voice channel (can be combined with 24/7 to always be in voice channel until everyone leaves; if 24/7 is on disconnectTime will add a disconnect delay after everyone leaves.)
 	debug: false, //- Debug mode
-	cookieSecret: "CodingWithSudhan is epic", //- Cookie Secret
+	cookieSecret: "ISSAM_FTN is epic", //- Cookie Secret
 	website: "http://localhost:4200", //- without the / at the end
 	// You need a lavalink server for this bot to work!!!!
 	// Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
@@ -37,11 +37,11 @@ module.exports = {
 	embedColor: "#2f3136", //- Color of the embeds, hex supported
 	presence: {
 		// PresenceData object | https://discord.js.org/#/docs/main/stable/typedef/PresenceData
-		status: "online", //- You can have online, idle, dnd and invisible (Note: invisible makes people think the bot is offline)
+		status: "dnd", //- You can have online, idle, dnd and invisible (Note: invisible makes people think the bot is offline)
 		activities: [
 			{
-				name: "Music", //- Status Text
-				type: "LISTENING", //- PLAYING, WATCHING, LISTENING, STREAMING
+				name: "Ur Music", //- Status Text
+				type: "STREAMING", //- PLAYING, WATCHING, LISTENING, STREAMING
 			},
 		],
 	},
